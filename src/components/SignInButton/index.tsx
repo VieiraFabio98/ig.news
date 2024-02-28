59e352948d9eede31a82419fcd41cc619dc7ed9f
VieiraFabio98 <vieirafabio5271@outlook.com>
2024-02-27 23:53:47 -0300

    Criando autenticação com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const {data: session} = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
     className={styles.signInButton}
     type="button"
+    onClick={() => {signOut()}}
     >
       <FaGithub color='04D361'/>
-      Fabio Vieira Alves
+      {session?.user?.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
     className={styles.signInButton}
     type="button"
+    onClick={() => {
+      signIn('gitHub')
+    }}
     >
       <FaGithub color='#EBA417'/>
       Sign in with GitHub
